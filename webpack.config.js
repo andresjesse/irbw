7bf664167829a9113c2871c83c74d0875eea6087
Andres <andres.porfirio@gmail.com>
2020-12-29 19:07:40 -0300

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,23 @@ module.exports = {
   devServer: {
     contentBase: "./dist",
   },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
 };
