cc26ad08be3cee1fd70803be87960c2b2d542a9c
Andres <andres.porfirio@gmail.com>
2021-01-18 18:47:31 -0300

    cleaned and documented water shader - foam concluded

--- src/materials/customWater/customWaterMaterial.js
+++ src/materials/customWater/customWaterMaterial.js
@@ -20,14 +20,16 @@ const createCustomWaterMaterial = (scene) => {
     varying vec3 vnormal;
 
     void main(void) {
-
+      // collect actual y position and normal
       yPos = position.y;
       vnormal = normal;
 
+      // calculate vertex position by overriding Y with water level (from terrain height)
       gl_Position = worldViewProjection * vec4(position.x, ${
         TerrainSegmentConfig.MIN_HEIGHT * 0.5
       }, position.z, 1.0);
 
+      // collect uv
       vUV = uv; 
     }
   `;
@@ -49,40 +51,40 @@ const createCustomWaterMaterial = (scene) => {
     uniform vec2 uWind;
     uniform float uWaterAlpha;
     uniform float uWaveDistortion;
+    uniform float uWaveScale;
     uniform vec3 uDiffuseLightColor;
 
     void main(void) {
-      
-      vec4 txNormal = texture2D(normalMap, vUV*uWaveDistortion + vec2(time*uWind.x, time*uWind.y) );
-      vec4 diffuse = texture2D(reflectionMap, vUV + vec2(txNormal.r, txNormal.g)*uWaveDistortion -vec2(time*uWind.x, time*uWind.y) );
+      // sample water normalmap and diffuse (sky reflection)
+      vec4 txNormal = texture2D(normalMap, vUV*uWaveScale + vec2(time*uWind.x, time*uWind.y) );
+      vec4 diffuse = texture2D(reflectionMap, vUV + vec2(txNormal.r, txNormal.g)*uWaveDistortion -vec2(time*uWind.x*2.0, time*uWind.y) );
 
+      // normalize yPos (from vertex) by Terrain Height
       float normalizedYPos = yPos/${TerrainSegmentConfig.MIN_HEIGHT.toFixed(2)};
 
+      // apply diffuse lighting
       diffuse = diffuse * vec4(uDiffuseLightColor, 1.0);
 
-      //angle between face and up vector gives water border
+      // angle between face and up vector gives water border
       float foamAlpha = acos(dot(normalize(vnormal), vec3(0,1.0,0)));
 
-      //foam effect: step 1 distortion + alpha
-      // vec2 foamDistortion = vec2(txNormal.r, txNormal.g)*0.03;
-      // vec4 txFoam = texture2D(foam, vUV*8.0 + foamDistortion ); 
-      // gl_FragColor = mix(vec4(txFoam.rgb , foamAlpha), vec4(1,0,0,1), 1.0-foamAlpha*foamAlpha);
-
-      //step 2: motion vector with sin/cos loop
-      // vec2 foamNormalWave = vec2(vnormal.x*sin(time*0.1), vnormal.z*sin(time*0.1))*0.05; //sin sin pulse, sin cos wave
-      // vec4 txFoam = texture2D(foam, vUV*8.0 + foamNormalWave ); 
-      // gl_FragColor = mix(vec4(txFoam.rgb , foamAlpha), vec4(1,0,0,1), 1.0-foamAlpha*foamAlpha);
-
-      //step 3: foam distortion + wave (red debug)
-      diffuse = vec4(1,0,0,1);
-
+      // calculate foam distortion and wave
       vec2 foamDistortion = vec2(txNormal.r, txNormal.g)*0.03;
       vec2 foamNormalWave = vec2(vnormal.x*sin(time*0.1), vnormal.z*cos(time*0.1))*0.05; 
+
+      // clamp border for smoothness: pow( y_normal, factor)
+      //    the bigger is y_normal the near from terrain border foam will be
+      //    the bigger is factor the thinner is the "smoothed" border
+      //    result is clamped by uWaterAlpha uniform
       float normalBorderAlpha = clamp( pow(normalizedYPos*1.4, 8.0), 0.0, uWaterAlpha);
 
+      // generate foam texture from combined UV (8.0 is foam image scale)
       vec4 txFoam = texture2D(foam, vUV*8.0 + foamNormalWave + foamDistortion); 
 
+      // combine diffuse and foam
       vec4 diffuseWithFoam = mix(vec4(txFoam.rgb , foamAlpha), diffuse, 1.0-foamAlpha );
+
+      // apply clamped border as alpha
       diffuseWithFoam.a = normalBorderAlpha;
 
       gl_FragColor = diffuseWithFoam;
@@ -114,6 +116,7 @@ const createCustomWaterMaterial = (scene) => {
   customWaterMaterial.setArray2("uWind", [-0.0015, -0.002]);
   customWaterMaterial.setFloat("uWaterAlpha", 0.45);
   customWaterMaterial.setFloat("uWaveDistortion", 0.3);
+  customWaterMaterial.setFloat("uWaveScale", 6.0);
 
   let time = 0;
   scene.registerBeforeRender(() => {
