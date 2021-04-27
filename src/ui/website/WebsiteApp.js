bbbf7a15e4666d3bfcd452a7f7ff7d886ef86e1c
Andres <andres.porfirio@gmail.com>
2021-04-27 10:10:05 -0300

    built production

--- src/ui/website/WebsiteApp.js
+++ src/ui/website/WebsiteApp.js
@@ -6,6 +6,11 @@ const WebsiteApp = function () {
     <>
       <h1>IrrRPG Builder 2.0w</h1>
 
+      <p>
+        Soon you'll see a beautiful home page here.. for now, you can directly
+        access our Editor tech demo (no sign-up required).
+      </p>
+
       <a href="editor.html">Editor Login</a>
     </>
   );
